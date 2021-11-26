8a1d3521ba0cffd6f62f5b3678d3baa3d359e960
Borja <borjius@gmail.com>
2021-11-26 12:22:47 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+const App = () => {
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: 'white',
+      justifyContent: 'center',      
+    }}>
+      <Text style={{
+        fontSize: 45,
+        textAlign:'center'
+      }}>Hola desde mi movil molon</Text>
+    </View>
+  )
+}
+
+export default App;
